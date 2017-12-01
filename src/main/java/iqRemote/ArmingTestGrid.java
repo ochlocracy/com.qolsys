03f31c2e78e27c1d223afa746af46c57f77e4f6d
ZachPulling <zachary.pulling@qolsys.com>
2017-12-01 14:34:09 -0800

    Test

--- src/main/java/iqRemote/ArmingTestGrid.java
+++ src/main/java/iqRemote/ArmingTestGrid.java
@@ -24,6 +24,7 @@ public class ArmingTestGrid {
         ConfigProps.init();
         SensorsActivity.init();
     }
+    ///testZach
 
     public AndroidDriver<WebElement> getDriver() {
         return driver;
