d1ac3ec441b6215b6e2285b38a5209793e12ee73
ZachPulling <zachary.pulling@qolsys.com>
2018-01-11 15:15:01 -0800

    DisarmPhotos

--- src/main/java/iqRemote/DisarmPhotos.java
+++ src/main/java/iqRemote/DisarmPhotos.java
@@ -7,21 +7,22 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import panel.PanelCameraPage;
 import utils.ConfigProps;
+import utils.Setup;
 
 import java.io.IOException;
-
 
 public class DisarmPhotos extends SetupRemote {
 
     EventContains EventContaints = new EventContains();
-    String logcat = "/home/qolsys/IdeaProjects/comqolsys2017/log/test.txt";
+    Setup setup = new Setup();
 
     public DisarmPhotos() throws Exception {
+        ConfigProps.init();
     }
 
     @BeforeMethod
     public void setupDriver() throws Exception {
-        setup_driver("6NJUMEQPGZ", "http://127.0.1.1", "4723");
+        setup_driver("6NJUM6KOWF", "http://127.0.1.1", "4723");
         // deleteLogFile(logcat);
     }
 
@@ -31,8 +32,8 @@ public class DisarmPhotos extends SetupRemote {
         System.out.println("Delete_All_Photos Begin");
         Thread.sleep(15000);
 
-        swipeFromRighttoLeft(); //check
-        
+        swipeFromLefttoRight(); //check
+
         Thread.sleep(3000);
         try {
             while (camera.Photo_lable.isDisplayed()) {
@@ -46,7 +47,8 @@ public class DisarmPhotos extends SetupRemote {
         }
         swipeFromRighttoLeft();
         Thread.sleep(3000);
-        rt.exec(ConfigProps.adbPath + " ls -l /storage/sdcard0/DisarmPhotos | busybox1.11  wc -l");
+        String process = ConfigProps.adbPath + " shell ls -l /storage/sdcard0/DisarmPhotos | busybox1.11  wc -l";
+        setup.execCmd(process);
     }
 
     @Test(priority = 2)
@@ -54,11 +56,9 @@ public class DisarmPhotos extends SetupRemote {
         System.out.println("Add_All_Photos Begin");
 
         Thread.sleep(15000);
-        driver.findElement(By.id("com.qolsys:id/t3_img_disarm")).click();
         driver.findElement(By.id("com.qolsys:id/img_arm_stay")).click();
-        Thread.sleep(10000);
         driver.findElement(By.id("com.qolsys:id/t3_img_disarm")).click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         driver.findElement(By.id("com.qolsys:id/tv_keyOne")).click();
         driver.findElement(By.id("com.qolsys:id/tv_keyTwo")).click();
         driver.findElement(By.id("com.qolsys:id/tv_keyThree")).click();
@@ -66,6 +66,7 @@ public class DisarmPhotos extends SetupRemote {
         Thread.sleep(5000);
 
         //do a stop when it hits 21
+        //would i have to check image count, compare variables then do a stop when its 21
         //maybe do a verify that it overwrites the last pic
     }
 
@@ -73,7 +74,19 @@ public class DisarmPhotos extends SetupRemote {
     public void checkPhotosAdded() throws Exception {
         System.out.println("Verify Added Photos Begin");
 
-        rt.exec(ConfigProps.adbPath + " ls -l /storage/sdcard0/DisarmPhotos | busybox1.11  wc -l");
+        Process exec = rt.exec(ConfigProps.adbPath + " ls -l /storage/sdcard0/DisarmPhotos | busybox1.11  wc -l");
+        System.out.println(exec.getOutputStream().toString());
+        System.out.println("Verify Added Photos Finished");
+
+
+        //File dir = new File(Environment.getExternalStorageDirectory()
+        //+ "/images");
+        //File[] files = dir.listFiles();
+        //int numberOfImages=files.length;
+
+
+        //on main panel
+        //on ADC
     }
 
     @AfterMethod
@@ -88,7 +101,7 @@ public class DisarmPhotos extends SetupRemote {
 //The idea is to be able to send the event from the IQRemote panel
 //and do the verification on the Primary panel and on the ADC dealer website.
 
-//shell commands, see how many files their are
+//shell commands, see how many files their are CHECK
 //max of 20 images will be stored.
 //creating 21st will overwrite.
 
